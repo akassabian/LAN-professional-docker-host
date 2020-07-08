c31110471c54eaa7ddf821d5b193741db7450383
AJ <alainkassabian@gmail.com>
2020-07-08 16:28:15 -0400

    make rows full width, restyle cta-button area

--- web-alainkassabian.com/wordpress/wp-content/themes/ajk/index.php
+++ web-alainkassabian.com/wordpress/wp-content/themes/ajk/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 	
-	<div class="container content-main">
+	<div class="container-fluid content-main">
 
 					<div class="row">						
                         <div class="col-6 d-flex bio_pic_col">
@@ -12,7 +12,7 @@
 						<p>I grew up in Whitby, Ontario. I'm an autodidact, polymath that gravitates towards technology, art, health, and social systems. I began developing websites professionaly near the end of 2018.</p>
 		</div>
 </div>
-		<div class="row">
+		<div class="row row-cta">
 			<div class="col">
 				<p class="text-center">
 					Need help on a project?
