a8e4c015bfd8fed4b1eabb07d4c1f58c4c3fbd94
AJ <alainkassabian@gmail.com>
2020-07-08 16:10:37 -0400

    re-organize bio content and cta

--- web-alainkassabian.com/wordpress/wp-content/themes/ajk/index.php
+++ web-alainkassabian.com/wordpress/wp-content/themes/ajk/index.php
@@ -3,19 +3,24 @@
 	<div class="container content-main">
 
 					<div class="row">						
-                        <div class="col-3">
-						   <img class="display_pic" src="<?php echo get_template_directory_uri(); ?>/images/display_pic.jpg" width="150px" height="150px">
+                        <div class="col-6 d-flex bio_pic_col">
+						   <img class="display_pic" src="<?php echo get_template_directory_uri(); ?>/images/display_pic.jpg" width="250px" height="250px">
 						</div>
-						<div class="col-9">
+						<div class="col-6">
 						<p>Hello! My name is Alain.</p>
 						<p>I am a Missisauga based web developer.</p>
 						<p>I grew up in Whitby, Ontario. I'm an autodidact, polymath that gravitates towards technology, art, health, and social systems. I began developing websites professionaly near the end of 2018.</p>
-
-						<p>Need help on a project?</p>
-
-						<p><a>Free Consultation</a></p>
+		</div>
 </div>
-					</div>
+		<div class="row">
+			<div class="col">
+				<p class="text-center">
+					Need help on a project?
+				<br>
+				<a href="#" class="ajk-button">Free Consultation</a>
+			</div>
+        </div>
+	</div>
 		
 
 
