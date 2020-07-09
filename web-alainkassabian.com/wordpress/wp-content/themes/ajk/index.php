54d4fa4185afe45cb930676cd899d835eeb3c147
AJ <alainkassabian@gmail.com>
2020-07-09 11:00:39 -0400

    update text, add icon/topic area, adjust background color structure, add spacing to index

--- web-alainkassabian.com/wordpress/wp-content/themes/ajk/index.php
+++ web-alainkassabian.com/wordpress/wp-content/themes/ajk/index.php
@@ -1,32 +1,57 @@
 <?php get_header(); ?>
-	
-	<div class="container-fluid content-main">
 
-					<div class="row row-bio">						
-                        <div class="col-6 d-flex bio_pic_col">
-						   <img class="display_pic" src="<?php echo get_template_directory_uri(); ?>/images/display_pic.jpg" width="250px" height="250px">
-						</div>
-						<div class="col-6">
-						<p>Hello! My name is Alain.</p>
-						<p>I am a Missisauga based web developer.</p>
-						<p>I grew up in Whitby, Ontario. I'm an autodidact, polymath that gravitates towards technology, art, health, and social systems. I began developing websites professionaly near the end of 2018.</p>
-		</div>
-</div>
-
-<ul class="row row-topics">
-		<li class="col-6">Web Resume</li>
-		<li class="col-6">Technology</li>
-		<li class="col-6">Health</li>
-	</ul>
-
-		<div class="row row-cta">
-			<div class="col">
-				<p class="text-center">
-					Need help on a project?
-				<br>
-				<a href="#" class="ajk-button" style=>Free Consultation</a>
+	<div class="bg-ajk-dark">	
+		<div class="container content-main">
+			<div class="row row-bio">						
+				<div class="col-6 d-flex bio_pic_col">
+					<img class="display_pic" src="<?php echo get_template_directory_uri(); ?>/images/display_pic.jpg" width="250px" height="250px">
+				</div>
+				<div class="col-6">
+					<p>Hello! My name is Alain.</p>
+					<p>I am a Mississauga based web developer.</p>
+					<p>I grew up in Whitby, Ontario. I'm an autodidactic polymath (self taught on broad range of topics) that gravitates towards technology, art, health, and social systems. I began developing websites professionaly near the end of 2018.</p>
+				</div>
 			</div>
-        </div>
+		</div>
+
+	
+		<div class="container">
+			<ul class="row row-topics">
+				<li class="col-6 d-flex">
+					<div class="topics-container">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/web-resume.svg">
+						<span>Web Resume</span>
+					</div>
+				</li>
+				<li class="col-6 d-flex">
+					<div class="topics-container">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/tech.svg">
+						<span>Technology</span>
+					</div>
+				</li>
+				<li class="col-3"></li>
+				<li class="col-6 d-flex">
+					<div class="topics-container">
+						<img src="<?php echo get_template_directory_uri(); ?>/images/health.svg">						
+						<span>Health</span>
+					</div>
+				</li>
+				<li class="col-3"></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="bg-ajk-primary">
+		<div class="container">
+			<div class="row row-cta">
+				<div class="col">
+					<p class="text-center">
+						Need help on a project?
+					<br>
+					<a href="#" class="ajk-button" style=>Free Consultation</a>
+				</div>
+			</div>
+		</div>
 	</div>
 
 
