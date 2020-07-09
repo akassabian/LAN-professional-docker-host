ce3c4a31e98e16bfe29425b4baf8c75837f2c330
AJ <alainkassabian@gmail.com>
2020-07-09 08:57:36 -0400

    add category blocks to home page

--- web-alainkassabian.com/wordpress/wp-content/themes/ajk/index.php
+++ web-alainkassabian.com/wordpress/wp-content/themes/ajk/index.php
@@ -2,7 +2,7 @@
 	
 	<div class="container-fluid content-main">
 
-					<div class="row">						
+					<div class="row row-bio">						
                         <div class="col-6 d-flex bio_pic_col">
 						   <img class="display_pic" src="<?php echo get_template_directory_uri(); ?>/images/display_pic.jpg" width="250px" height="250px">
 						</div>
@@ -12,15 +12,24 @@
 						<p>I grew up in Whitby, Ontario. I'm an autodidact, polymath that gravitates towards technology, art, health, and social systems. I began developing websites professionaly near the end of 2018.</p>
 		</div>
 </div>
+
+<ul class="row row-topics">
+		<li class="col-6">Web Resume</li>
+		<li class="col-6">Technology</li>
+		<li class="col-6">Health</li>
+	</ul>
+
 		<div class="row row-cta">
 			<div class="col">
 				<p class="text-center">
 					Need help on a project?
 				<br>
-				<a href="#" class="ajk-button">Free Consultation</a>
+				<a href="#" class="ajk-button" style=>Free Consultation</a>
 			</div>
         </div>
 	</div>
+
+
 		
 
 
