0cc751d5610173f0e7af232b47bbc56ed24da344
AJ <alainkassabian@gmail.com>
2020-07-15 06:09:39 -0400

    remove unused codes, adjust content-main padding, column sizing, typography, footer colors on single-post and category templates

--- web-alainkassabian.com/wordpress/wp-content/themes/ajk/category.php
+++ web-alainkassabian.com/wordpress/wp-content/themes/ajk/category.php
@@ -3,35 +3,23 @@
 	<div class="bg-ajk-dark">	
 		<div class="container content-main">
 			<div class="row">						
-                <div class="col-12 col-md-2">
-                    <?php /* 
-                    $categories =  get_categories();  
-                    foreach  ($categories as $category) if ($tmp++ < 3) {
-                        //Display the category information using $category values like $category->cat_name
-                            echo '<h2 class="toc-headers">'.$category->name.'</h2>';
-                            echo '<ul id="toc-items">';
-                                $args = array( 'category' => $category->term_id, 'post_type' =>  'post', 'posts_per_page' => -1, 'post_status' => 'publish');
-                                foreach (get_posts($args) as $post) {
-                                    setup_postdata( $post );
-                                    echo '<li><a href="'.get_permalink($post->ID).'">'.get_the_title().'</a></li>';   
-                                }  
-                            echo '</ul>';
-                        }*/
-                    ?>                    
+                <div class="col-12 col-md-3 similar-blogs">              
                     <?php
                     $category_id = get_queried_object()->term_id;
                     $args = array( 'category' => $category_id, 'post_type' =>  'post', 'posts_per_page' => -1, 'post_status' => 'publish' );
-                    $postslist = get_posts($args);
-                    foreach ($postslist as $post) :
-                        setup_postdata($post); ?>
-                        <h2><a class="post-title"  href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <?php endforeach; ?>
+                    $postslist = get_posts($args);?>
+                    <ul>
+                        <?php foreach ($postslist as $post) :
+                            setup_postdata($post); ?>
+                            <li><a class="post-title"  href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                        <?php endforeach; ?>
+                    </ul>
                 </div>
-                <div class="col-12 col-md-10">
+                <div class="col-12 col-md-9">
                     <?php if (have_posts()) : ?>
                         <?php while (have_posts()) :
                             the_post(); ?>
-                            <h1 class="text-center"><?php the_title(); ?></h1>
+                            <h1><?php the_title(); ?></h1>
                             <?php the_content(); ?>
                         <?php endwhile; ?>
                     <?php endif; ?>
--- web-alainkassabian.com/wordpress/wp-content/themes/ajk/single.php
+++ web-alainkassabian.com/wordpress/wp-content/themes/ajk/single.php
@@ -3,21 +3,23 @@
 	<div class="bg-ajk-dark">	
 		<div class="container content-main">
 			<div class="row">						
-                <div class="col-12 col-md-2">          
+                <div class="col-12 col-md-3 similar-blogs">          
                     <?php
                     $category_id = get_the_category(get_queried_object()->ID)[0]->term_id;
                     $args = array( 'category' => $category_id, 'post_type' =>  'post', 'posts_per_page' => -1, 'post_status' => 'publish' );
-                    $postslist = get_posts($args);
-                    foreach ($postslist as $post) :
-                        setup_postdata($post); ?>
-                        <h2><a class="post-title"  href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <?php endforeach; ?>
+                    $postslist = get_posts($args);?>
+                    <ul>
+                        <?php foreach ($postslist as $post) :
+                            setup_postdata($post); ?>
+                            <li><a class="post-title"  href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                        <?php endforeach; ?>
+                    </ul>
                 </div>
-                <div class="col-12 col-md-10">
+                <div class="col-12 col-md-9">
                     <?php if (have_posts()) : ?>
                         <?php while (have_posts()) :
                             the_post(); ?>
-                            <h1 class="text-center"><?php the_title(); ?></h1>
+                            <h1><?php the_title(); ?></h1>
                             <?php the_content(); ?>
                         <?php endwhile; ?>
                     <?php endif; ?>
